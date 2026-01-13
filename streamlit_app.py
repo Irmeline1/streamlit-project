adc42db77ce2524d0efeb7e114901c5e555cc07f
IrmelineGuevou <mguevou@gmail.com>
2026-01-13 16:42:53 +0100

    initial streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# Ton message de test
+st.write('HelloMBADIA !')
+
+# Ajoute ceci pour ta présentation, ça fera plus pro :
+
+st.write("Ceci est ma première application interactive connectée à Snowflake.")
+
